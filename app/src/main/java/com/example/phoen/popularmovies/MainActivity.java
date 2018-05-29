b875ec3e2de094b83c0338b891252a5e92b879ea
q00u <github@korv.us>
2018-05-29 16:03:53 -0700

    Changing api keys location

--- app/src/main/java/com/example/phoen/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/phoen/popularmovies/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.phoen.popularmovies;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        return (null!=cm.getActiveNetworkInfo() && cm.getActiveNetworkInfo().isConnectedOrConnecting());
     }
 }
